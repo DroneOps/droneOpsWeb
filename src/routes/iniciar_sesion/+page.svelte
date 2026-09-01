f0a025c0cf4b229130d5697efa5e0e15ba493954
Charlie1719 <A01640751@tec.mx>
2026-09-01 12:08:34 -0600

    inicio sesion basico

---
+++ src/routes/iniciar_sesion/+page.svelte
@@ -0,0 +1,18 @@
+
+
+<h1>Iniciar Sesión</h1>
+
+<form method="POST">
+	<label>
+		Correo electrónico:
+		<input type="email" name="email" required />
+	</label>
+
+	<label>
+		Contraseña:
+		<input type="password" name="password" required />
+	</label>
+
+	<button type="submit">Entrar</button>
+</form>
+
